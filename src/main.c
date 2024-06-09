70d4642cae45ebae2cb14d2307aa7f925c8abb35
duccdev <usr.bin.ducc.2@gmail.com>
2024-06-09 17:11:32 +0300

    fix: logging/formatting

--- src/main.c
+++ src/main.c
@@ -4,20 +4,22 @@
 #include <sys/types.h>
 #include <unistd.h>
 
-int main(void) {
-  if (getpgid(1) >= 0 && getpid() != 1) {
+int main(void)
+{
+  if (getpgid(1) >= 0 && getpid() != 1)
+  {
     return 1;
   }
 
-  log_info("Welcome to \033[1;34mNox\033[0m!\n");
+  printf("Welcome to \033[1;34mNox\033[0m!\n");
 
-  log_info("\n--- Stage 1 ---\n");
+  printf("\n--- Stage 1 ---\n");
   sysprep();
 
-  log_info("\n--- Stage 2 ---\n");
+  printf("\n--- Stage 2 ---\n");
   if (access("/bin/ash", F_OK) == -1)
     log_fatal("/bin/ash not found, please install a shell!");
 
-  log_info("Starting /bin/ash");
+  printf("Starting /bin/ash");
   execl("/bin/ash", "/bin/ash", NULL);
 }
